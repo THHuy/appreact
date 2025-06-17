c86289122b7b0109d8fc931d414ba88e0f30364a
TruongHoangHuy <truonghoanghuy1312@gmail.com>
2025-06-17 20:16:59 +0700

    Test Jenkins webhook

--- src/App.js
+++ src/App.js
@@ -6,18 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and Jenkins Test Docker Hub Login and Cloudflare
-        </a> */}
         <Hello />
       </header>
     </div>
--- src/components/__tests__/Hello.test.js
+++ src/components/__tests__/Hello.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Hello from '../Hello';
 
 test('Hiển thị tên đúng', () => {
-  render(<Hello name="Huy" />);
+  render(<Hello />);
+  const input = screen.getByPlaceholderText('Nhập tên của bạn');
+  fireEvent.change(input, { target: { value: 'Huy' } });
+
   expect(screen.getByText('Hello, Huy!')).toBeInTheDocument();
 });
