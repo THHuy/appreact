542299a2bb92cf9b3b529c637cd01f2c2d10cd89
TruongHoangHuy <truonghoanghuy1312@gmail.com>
2025-06-17 14:52:37 +0700

    Test Jenkins

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -1,14 +1,15 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import Counter from '../Counter';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "../Counter";
 
-test('Ban đầu count = 0', () => {
+test("Ban đầu count = 0", () => {
   render(<Counter />);
-  expect(screen.getByTestId('count')).toHaveTextContent('Count: 0');
+  expect(screen.getByTestId("count")).toHaveTextContent("Count: 0");
 });
 
-test('Tăng count khi bấm nút', () => {
+test("Tăng count khi bấm nút", () => {
   render(<Counter />);
-  const button = screen.getByText('Increase');
+  const button = screen.getByText("Increase");
   fireEvent.click(button);
-  expect(screen.getByTestId('count')).toHaveTextContent('Count: 1');
+  expect(screen.getByTestId("count")).toHaveTextContent("Count: 1");
 });
