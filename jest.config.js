e6ea24a24bea0c2e9f24ab4fca63361ddbb087b2
TruongHoangHuy <truonghoanghuy1312@gmail.com>
2025-06-17 14:56:56 +0700

    Test Jenkins

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 // jest.config.js
 module.exports = {
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    'reportWebVitals.js'
-  ],
-  testEnvironment: 'jsdom',
+  coveragePathIgnorePatterns: ["/node_modules/", "reportWebVitals.js"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(svg|jpg|jpeg|png|gif|webp|css|scss)$":
+      "<rootDir>/__mocks__/fileMock.js",
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
 };
